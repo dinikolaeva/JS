baadcd6a4d0e8d1f2a8717d888e3dcf21d3b99ad
Dilyana Nikolaeva <76437069+dinikolaeva@users.noreply.github.com>
2021-06-02 11:42:47 +0300

    Delete

--- JS-Advanced/01.Syntax,FunctionsAndStatements-Lab/01.EchoFunction.js/01.EchoFunction.js
+++
@@ -1,6 +0,0 @@
-function echo(someString) {
-    console.log(someString.length)
-    console.log(someString)
-}
-
-echo('strings are easy')
--- JS-Advanced/01.Syntax,FunctionsAndStatements-Lab/02.StringLength/02.StringLength.js
+++
@@ -1,9 +0,0 @@
-let sum = 0;
-
-function stringLength(first, second, third) {
-    sum = first.length + second.length + third.length;
-    console.log(sum);
-    console.log(Math.floor(sum/3));
-}
-
-stringLength('pasta', '5', '22.3');
