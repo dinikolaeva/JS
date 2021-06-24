b9522b8c3c34725c5bf0a69427d65264f2c8180b
Dilyana Nikolaeva <76437069+dinikolaeva@users.noreply.github.com>
2021-06-24 14:25:28 +0300

    Lab

---
+++ JS-Advanced/03.ArraysAndNestedArrays-Lab/01.EvenPositionElements/01.EvenPositionElements.js
@@ -0,0 +1,15 @@
+function evenPositions(arr) {
+
+    let result = '';
+
+    for (let i = 0; i < arr.length; i += 2) {
+
+        result += arr[i];
+        result += ' ';
+    }
+
+    console.log(result);
+}
+
+evenPositions(['20', '30', '40', '50', '60']);
+evenPositions(['5', '10']);
