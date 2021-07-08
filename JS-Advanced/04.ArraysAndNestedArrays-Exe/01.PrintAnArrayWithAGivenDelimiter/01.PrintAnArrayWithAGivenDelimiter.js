c972cff2e9e5f31dc387ae2dd6b8fcdaabc001f5
Dilyana Nikolaeva <76437069+dinikolaeva@users.noreply.github.com>
2021-07-08 09:05:56 +0300

    Exe

---
+++ JS-Advanced/04.ArraysAndNestedArrays-Exe/01.PrintAnArrayWithAGivenDelimiter/01.PrintAnArrayWithAGivenDelimiter.js
@@ -0,0 +1,22 @@
+function delimiter(arr, delimiter){
+
+    console.log(arr.join(delimiter));
+}
+
+delimiter(
+    ['One', 
+'Two', 
+'Three', 
+'Four', 
+'Five'], 
+'-'
+);
+
+delimiter(['How about no?', 
+'I',
+'will', 
+'not', 
+'do', 
+'it!'], 
+'_'
+);
