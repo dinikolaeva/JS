3100a30860f95445ad59aa49daca60629faaf5e6
Dilyana Nikolaeva <76437069+dinikolaeva@users.noreply.github.com>
2021-07-15 10:55:09 +0300

    Exe

---
+++ JS-Advanced/04.ArraysAndNestedArrays-Exe/08.SortАnArrayBy2Criteria/08.SortАnArrayBy2Criteria.js
@@ -0,0 +1,24 @@
+function sort(arr) {
+    return arr.sort((a, b) => {
+
+        if (a.length == b.length) {
+            return a.localeCompare(b);
+        }
+        return a.length - b.length;
+    }).join('\n');
+}
+
+console.log(sort(['alpha',
+    'beta',
+    'gamma']));
+
+console.log(sort(['Isacc',
+    'Theodor',
+    'Jack',
+    'Harrison',
+    'George']));
+
+console.log(sort(['test',
+    'Deny',
+    'omen',
+    'Default']));
