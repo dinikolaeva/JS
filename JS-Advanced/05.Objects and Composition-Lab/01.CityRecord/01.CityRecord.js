e5c6e12777903eaac489e292c5b2170d632ff2d7
Dilyana Nikolaeva <76437069+dinikolaeva@users.noreply.github.com>
2021-07-19 09:23:15 +0300

    Lab

---
+++ JS-Advanced/05.Objects and Composition-Lab/01.CityRecord/01.CityRecord.js
@@ -0,0 +1,12 @@
+function city(name, population, treasury) {
+    let obj = {
+        name,
+        population,
+        treasury
+    };
+
+    return obj;
+}
+
+console.log(city('Tortuga', 7000, 15000));
+console.log(city('Santo Domingo', 12000, 23500));
