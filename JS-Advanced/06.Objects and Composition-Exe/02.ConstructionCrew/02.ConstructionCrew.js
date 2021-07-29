86d261ccd7898385fb4111b0b3fd78a75effc2ce
Dilyana Nikolaeva <76437069+dinikolaeva@users.noreply.github.com>
2021-07-29 15:19:13 +0300

    Exe

---
+++ JS-Advanced/06.Objects and Composition-Exe/02.ConstructionCrew/02.ConstructionCrew.js
@@ -0,0 +1,36 @@
+function crew(worker) {
+    if (worker.dizziness) {
+        worker.levelOfHydrated += 0.1 * worker.weight * worker.experience;
+        worker.dizziness = false;
+    }
+
+    return worker;
+}
+
+console.log(crew(
+    {
+        weight: 80,
+        experience: 1,
+        levelOfHydrated: 0,
+        dizziness: true
+    }
+));
+
+console.log(crew(
+    {
+        weight: 120,
+        experience: 20,
+        levelOfHydrated: 200,
+        dizziness: true
+    }
+
+))
+
+console.log(crew(
+    {
+        weight: 95,
+        experience: 3,
+        levelOfHydrated: 0,
+        dizziness: false
+    }
+));
