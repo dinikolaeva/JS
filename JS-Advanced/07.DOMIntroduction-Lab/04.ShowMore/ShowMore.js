b8ff5556817b92b6dee3eb6860f1e41c28d3fb35
Dilyana Nikolaeva <76437069+dinikolaeva@users.noreply.github.com>
2021-08-10 15:04:13 +0300

    Lab

---
+++ JS-Advanced/07.DOMIntroduction-Lab/04.ShowMore/ShowMore.js
@@ -0,0 +1,6 @@
+function showText() {
+    const removeElement = document.getElementById('more');
+    removeElement.style.display = 'none';
+    const element = document.getElementById('text');
+    element.style.display = 'inline';
+}
