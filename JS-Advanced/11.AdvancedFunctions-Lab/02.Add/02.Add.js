8c3f2e2fcdb8a25c2b437157f3392e685b4974e9
Dilyana Nikolaeva <76437069+dinikolaeva@users.noreply.github.com>
2021-09-24 21:18:58 +0300

    Lab

---
+++ JS-Advanced/11.AdvancedFunctions-Lab/02.Add/02.Add.js
@@ -0,0 +1,16 @@
+function solution(num) {
+
+    return add5;
+
+    function add5(addValue) {
+        return result = num + addValue;
+    }
+}
+
+let add5 = solution(5);
+console.log(add5(2));
+console.log(add5(3));
+
+let add7 = solution(7);
+console.log(add7(2));
+console.log(add7(3));
