6c1b4c0a55321c6fb785165141f516e33aaea526
Dilyana Nikolaeva <76437069+dinikolaeva@users.noreply.github.com>
2021-11-05 09:42:12 +0200

    Lab

---
+++ JS Applications/01.HTTPAndRESTServices-Lab/03.GithubCreateIssue/GithubCreateIssue.js
@@ -0,0 +1,15 @@
+// POST: https://api.github.com/repos/testnakov/test-nakov-repo/issues
+
+// go Authorization -> Basic Auth -> username and password in GitHub -> Body ->
+
+// text of body in JSON:
+
+{
+    "title": "I have created an isshue",
+    "body": "Minor bug",
+    "labels": ["bug", "minor"]
+}
+
+// Send
+
+// not working in GitHub, not allowed in GitHub anymore
