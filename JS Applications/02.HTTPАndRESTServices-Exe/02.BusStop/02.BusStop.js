68a812871b56a4b5151ca481bcd91b86528bf933
Dilyana Nikolaeva <76437069+dinikolaeva@users.noreply.github.com>
2021-11-08 13:29:31 +0200

    Exe

---
+++ JS Applications/02.HTTPАndRESTServices-Exe/02.BusStop/02.BusStop.js
@@ -0,0 +1,16 @@
+// GET: http://localhost:3030/jsonstore/bus/businfo/1287 where 1287 is stopID of this bus
+
+// return JSON:
+
+{
+    "buses": {
+        "76": 15,
+        "84": 10,
+        "204": 10,
+        "213": 18,
+        "280": 9,
+        "306": 31,
+        "604": 11
+    },
+    "name": "Orlov Most sq."
+}
